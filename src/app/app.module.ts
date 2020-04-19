3eeb724ab0536ada2799c3a618614ae6e2860641
donkeykong91 <darylnator@gmail.com>
2020-04-19 06:04:14 -0700

    initial

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactivFormsModule } from '@angular/forms';
+
+import { SandoAppComponent } from './sando-app.component';
+import { OrderSheetComponent } from './counter/order-sheet/order-sheet.component';
+import { DebugPanelComponent } from './counter/debug-panel/debug-panel.component';
+
+@NgModule({
+  imports: [
+    BrowserModule
+  ],
+  declarations: [
+    SandoAppComponent,
+    OrderSheetComponent,
+    DebugPanelComponent
+  ],
+  bootstrap: [
+    SandoAppComponent
+  ]
+})
+export class AppModule { }
