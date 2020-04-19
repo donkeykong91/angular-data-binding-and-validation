ccaf331d30301a6d762b290f9447ec4fc2b5d295
donkeykong91 <darylnator@gmail.com>
2020-04-19 07:00:29 -0700

    added new form controls

--- src/app/counter/order-sheet/order-sheet.component.ts
+++ src/app/counter/order-sheet/order-sheet.component.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'order-sheet',
   templateUrl: 'app/counter/order-sheet/order-sheet.component.html',
   styleUrls: ['app/counter/order-sheet/order-sheet.component.css']
 })
-export class OrderSheetComponent {  
+export class OrderSheetComponent {
   orderSheetForm: FormGroup;
+
   constructor(private formBuilder: FormBuilder) {
     this.buildForm();
-   }
-  buildForm() {
-    this.orderSheetForm = this.formBuilder.group({});
+  }
+  
+  private buildForm() {
+    this.orderSheetForm = this.formBuilder.group({
+      customerName: this.formBuilder.control(null),
+      specialtySandwich: this.formBuilder.control(null),
+      otherNotes: this.formBuilder.control(null)
+    });
   }
 }
