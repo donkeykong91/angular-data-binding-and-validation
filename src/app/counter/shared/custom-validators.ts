6ce7933e3eef21a300b381ef92612c00f3faeae0
donkeykong91 <darylnator@gmail.com>
2020-04-24 04:40:30 -0700

    added custom requirements for selecting either bread or specialty sandwich

--- src/app/counter/shared/custom-validators.ts
+++ src/app/counter/shared/custom-validators.ts
@@ -3,8 +3,7 @@ export class CustomValidators {
         return (control) => {
             let required = control.get(requiredControlName).value;
             let toCheck = control.get(controlToCheckName).value;
-            return (required) || (toCheck && !required) ? null : {requiredWhen: true};
+            return (required) || (toCheck && !required) ? null : {requiredwhen: true};
         };
     }
-}
 }
